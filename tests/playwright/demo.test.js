739ba66b98ed1d0f8a4a3546dabc674c381778a5
Tian Feng <tian.feng@saucelabs.com>
2021-02-02 08:56:46 -0800

    Update playwright config example (#99)
    * Update playwright config example
    
    * add some comments
    
    * update docker name
    
    * fix pipeline
    
    * fix pipeline

--- tests/playwright/demo.test.js
+++ tests/playwright/demo.test.js
@@ -1,71 +1,21 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+/**
+ * Copyright Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const { it, expect } = require('@playwright/test');
 
-describe('Herokuapp Login Page', () => {
-  describe('Login page is constructed correctly', () => {
-    test('Page is available', async () => {
-      await page.goto('https://the-internet.herokuapp.com/login');
-      expect(await page.url()).toContain('login');
-      await sleep(2000)
-    });
-
-    test('Username is available', async () => {
-      const usernameElement = await page.$('input#username');
-      expect(usernameElement).not.toBeNull();
-      await sleep(2000)
-    });
-
-    test('Password is available', async () => {
-      const passwordElement = await page.$('input#password');
-      expect(passwordElement).not.toBeNull();
-      await sleep(2000)
-    });
-
-    test('Login button is available', async () => {
-      const loginButtonElement = await page.$('button[type="submit"]');
-      expect(loginButtonElement).not.toBeNull();
-      await sleep(2000)
-    });
-  });
-
-
-  describe.skip('Login scenarios', () => {
-    test('Bad credentials fail', async () => {
-      await page.goto('https://the-internet.herokuapp.com/login');
-      await page.type('input#username', 'junk');
-      await page.type('input#password', 'junk');
-      await page.click('button[type="submit"]');
-      const divAlert = await page.$('div#flash');
-      const alertText = await page.evaluate(divAlert => divAlert.textContent, divAlert);
-      expect(alertText).toContain("Your username is invalid!");
-      await sleep(2000)
-    });
-
-    test('Good credentials pass', async () => {
-      await page.goto('https://the-internet.herokuapp.com/login');
-      await page.type('input#username', 'tomsmith');
-      await page.type('input#password', 'SuperSecretPassword!');
-      await page.click('button[type="submit"]');
-      const divAlert = await page.$('div#flash');
-      expect(await page.url()).toContain('secure');
-      expect(divAlert).not.toBeNull();
-      await sleep(2000)
-    });
-  });
-
-  describe.skip('Logout scenario', () => {
-    test('Can logout successfully', async () => {
-      await page.goto('https://the-internet.herokuapp.com/login');
-      await page.type('input#username', 'tomsmith');
-      await page.type('input#password', 'SuperSecretPassword!');
-      await page.click('button[type="submit"]');
-      await page.click('a[href="/logout"]');
-      const divAlert = await page.$('div#flash');
-      const alertText = await page.evaluate(divAlert => divAlert.textContent, divAlert);
-      expect(await page.url()).toContain('login');
-      expect(alertText).toContain("You logged out of the secure area!");
-      await sleep(2000)
-    });
-  });
+it('is a basic test with the page', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  expect(await page.innerText('.navbar__title')).toBe('Playwright');
 });
